c7844b502cab2f26efd566f57242a9db73407638
ani1505 <118676630+ani1505@users.noreply.github.com>
2023-06-19 19:47:09 +0530

    Add files via upload

---
+++ personal/scripts.js
@@ -0,0 +1,8 @@
+// Add your JavaScript code here
+// You can include event listeners or other dynamic functionality
+
+// Example event listener
+document.addEventListener('DOMContentLoaded', function() {
+    // Code to be executed after the DOM is fully loaded
+  });
+  
